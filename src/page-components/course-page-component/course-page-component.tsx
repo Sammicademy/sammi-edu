980ae041f3b7bc998219af155c183229902e7dee
Samar <samarbadriddinov.tv@gmail.com>
2023-02-03 23:13:48 +0200

    Product animation

--- src/page-components/course-page-component/course-page-component.tsx
+++ src/page-components/course-page-component/course-page-component.tsx
@@ -1,15 +1,32 @@
 import { CoursePageComponentProps } from './course-page-component.props';
 import styles from './course-page-component.module.css';
 import { Advantages, Heading, HhData, Product, Sort, Tag, Text } from '../../components';
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import { sortReducer } from './sort.reducer';
 import { SortEnum } from '../../components/sort/sort.props';
+import { AnimatePresence } from 'framer-motion';
 
 const CoursePageComponent = ({ page, products }: CoursePageComponentProps): JSX.Element => {
 	const [state, dispatch] = useReducer(sortReducer, { sort: SortEnum.Rating, products: products });
 
 	const setSort = (sort: SortEnum) => {
 		dispatch({ type: sort });
+	};
+
+	useEffect(() => {
+		dispatch({ type: 'reset', initialState: products });
+	}, [products]);
+
+	const spring = {
+		type: 'spring',
+		stiffness: 500,
+		damping: 10,
+	};
+
+	const animations = {
+		initial: { scale: 0 },
+		animate: { scale: 1 },
+		exit: { scale: 1 },
 	};
 
 	return (
@@ -21,7 +38,10 @@ const CoursePageComponent = ({ page, products }: CoursePageComponentProps): JSX.
 			</div>
 
 			{/* PRODUCTS */}
-			<div>{state.products && state.products.map((c, idx) => <Product key={idx} product={c} />)}</div>
+			<AnimatePresence>
+				{state.products &&
+					state.products.map((c, idx) => <Product key={idx} layout transition={spring} {...animations} product={c} />)}
+			</AnimatePresence>
 
 			{/* VACATIONS */}
 			<div className={styles.hhTitle}>
